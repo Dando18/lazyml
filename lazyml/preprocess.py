b66b8dc428eb26955ad71f021273c33980163ec0
Daniel Nichols <dando18studios@gmail.com>
2023-02-21 16:16:23 -0500

    update preprocessing: (#4)
    - changed normalize to scale
    - added minmax_scale
    - added maxabs_scale
    - added normalize

--- lazyml/preprocess.py
+++ lazyml/preprocess.py
@@ -2,9 +2,11 @@
     author: Daniel Nichols
     date: February, 2022
 """
+# std imports
+from typing import Iterable
 
 
-def normalize(dataset, columns, **kwargs):
+def scale(dataset, columns: Iterable[str], **kwargs):
     from sklearn.preprocessing import StandardScaler
 
     scaler = StandardScaler()
@@ -14,7 +16,33 @@ def normalize(dataset, columns, **kwargs):
     dataset.test[columns] = scaler.transform(dataset.test[columns])
 
 
-def encode(dataset, columns, **kwargs):
+def minmax_scale(dataset, columns: Iterable[str], **kwargs):
+    from sklearn.preprocessing import MinMaxScaler
+
+    scaler = MinMaxScaler(**kwargs)
+    scaler.fit(dataset.train[columns])
+    dataset.train[columns] = scaler.transform(dataset.train[columns])
+    dataset.test[columns] = scaler.transform(dataset.test[columns])
+
+
+def maxabs_scale(dataset, columns: Iterable[str], **kwargs):
+    from sklearn.preprocessing import MaxAbsScaler
+
+    scaler = MaxAbsScaler(**kwargs)
+    scaler.fit(dataset.train[columns])
+    dataset.train[columns] = scaler.transform(dataset.train[columns])
+    dataset.test[columns] = scaler.transform(dataset.test[columns])
+
+
+def normalize(dataset, columns: Iterable[str], **kwargs):
+    from sklearn.preprocessing import Normalizer
+
+    scaler = Normalizer(**kwargs)
+    dataset.train[columns] = scaler.fit_transform(dataset.train[columns])
+    dataset.test[columns] = scaler.transform(dataset.test[columns])
+
+
+def encode(dataset, columns: Iterable[str], **kwargs):
     from sklearn.preprocessing import LabelEncoder
 
     for column in columns:
@@ -27,11 +55,11 @@ def encode(dataset, columns, **kwargs):
         dataset.test[column] = encoder.transform(dataset.test[column])
 
 
-def one_hot_encode(dataset, columns, **kwargs):
+def one_hot_encode(dataset, columns: Iterable[str], **kwargs):
     dataset.one_hot_encode(columns)
 
 
-def binarize(dataset, columns, threshold=0.0):
+def binarize(dataset, columns: Iterable[str], threshold=0.0):
     from sklearn.preprocessing import Binarizer
 
     binarizer = Binarizer(threshold=threshold)
@@ -42,6 +70,9 @@ def binarize(dataset, columns, threshold=0.0):
 
 
 PREPROCESSORS_MAP_ = {
+    "scale": scale,
+    "minmax_scale": minmax_scale,
+    "maxabs_scale": maxabs_scale,
     "normalize": normalize,
     "encode": encode,
     "one_hot_encode": one_hot_encode,
